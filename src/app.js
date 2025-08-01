2696188689c2eb5a33365417636fd974cee1dc2a
Noe Hsu <noe13hsu@gmail.com>
2025-08-01 12:04:21 +1000

    Add cookieParser and cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const userRoutes = require('./routes/user-routes')
 const transactionRoutes = require('./routes/transaction-routes')
 const categoryRoutes = require('./routes/category-routes')
@@ -7,6 +9,12 @@ const budgetRoutes = require('./routes/budget-routes')
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
 
 app.use((req, res, next) => {
   console.log(`[${req.method}] ${req.path}`);
