739b8b2a6de273ace69413baa02af2caefda4212
Noe Hsu <noe13hsu@gmail.com>
2025-08-01 11:53:22 +1000

    Add logout route

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const {getUser, login, register} = require('../controllers/user-controller')
 const {verifyToken} = require('../middleware/auth-middleware')
+const {getUser, login, logout, register} = require('../controllers/user-controller')
 
 const router = express.Router()
 
 router.get('/', verifyToken, getUser)
 router.post('/login', login)
+router.post('/logout', logout)
 router.post('/register', register)
 
 module.exports = router
