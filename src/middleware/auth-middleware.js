429bfe041968c1805ef6edfb0ec3feb3e2eac4d1
Noe Hsu <noe13hsu@gmail.com>
2025-08-01 11:58:07 +1000

    Get token from cookies instead

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -1,13 +1,11 @@
 const jwt = require('jsonwebtoken')
 
 exports.verifyToken = (req, res, next) => {
-  const authHeader = req.headers['authorization']
+  const token = req.cookies.token
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Invalid token' })
+  if (!token) {
+    return res.status(401).json({message: 'No token provided'})
   }
-
-  const token = authHeader.split(' ')[1]
 
   try {
     req.user = jwt.verify(token, process.env.JWT_SECRET)
